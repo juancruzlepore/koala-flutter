9a7dfa0639033428411fb7dcc20cb9f803eaa422
jcl <jlepore@itba.edu.ar>
2020-05-25 10:24:57 +0100

    try 2

--- main_node.js
+++ main_node.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 //setting middleware
-app.use(express.static(__dirname + '/build/web')); //Serves resources from public folder
-
+app.use(express.static(__dirname + '/build/web', { index: 'index.html' })); //Serves resources from public folder
 
 var server = app.listen(process.env.PORT || 8080);
