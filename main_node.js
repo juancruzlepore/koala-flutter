b096ae2f689e7f76574967e5f7f1a1a1979377ce
jcl <jlepore@itba.edu.ar>
2020-05-25 15:15:24 +0100

    - added cors package

--- main_node.js
+++ main_node.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 
 //setting middleware
@@ -6,10 +7,12 @@ app.use(express.static(__dirname + '/build/web', {
     index: 'index.html',
 })); //Serves resources from public folder
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+//app.use(function(req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "herokuapp.com"); // update to match the domain you will make the request from
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//  next();
+//});
+
+app.use(cors);
 
 var server = app.listen(process.env.PORT || 9090);
